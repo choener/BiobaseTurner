43ba42e77b32abb0be9d61aee53355260929b983
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-03-14 22:59:59 +0100

    generalized parsing of Vienna files

--- Biobase/Turner/Import/Vienna.hs
+++ Biobase/Turner/Import/Vienna.hs
@@ -10,7 +10,7 @@ import Control.Monad (guard)
 import Control.Applicative ( (<|>) )
 
 import Biobase.Turner.Types
-import Biobase.Turner.Import.Turner (minPP,maxPP,minPBB,maxPBB)
+import Biobase.Turner.Import.Turner (minPP,maxPP,minPBB,maxPBB,minPB,maxPB,minPPBB,maxPPBB,minPPBBB,maxPPBBB,minPPBBBB,maxPPBBBB)
 import Biobase.Types.Energy
 
 
@@ -29,30 +29,76 @@ blocksToTurner = error "blocksToTurner"
 -- corresponding parser where necessary.
 
 data Block
-  = BlockPP Text EE (Arr PP)
-  | BlockPBB Text EE (Arr PNN)
+  = BlockPP     Text EE (Arr PP)
+  | BlockPBB    Text EE (Arr PNN)
+  | BlockPB     Text EE (Arr PN)
+  | BlockPPBB   Text EE (Arr PPNN)
+  | BlockPPBBB  Text EE (Arr PPNNN)
+  | BlockPPBBBB Text EE (Arr PPNNNN)
 
 data EE = Entropy | Enthalpie
 
 type Arr i = Unboxed i DeltaDekaGibbs
 
 block :: Parser Block
-block = blockPP <|> blockPBB
+block = blockPP <|> blockPBB <|> blockPB <|> blockPPBB <|> blockPPBBB <|> blockPPBBBB
 
 blockPP :: Parser Block
-blockPP = (\(t,e) vs -> BlockPP t e vs) <$> (try $ headerParser blockHeader) <*> (ppArray >>= convertPP) <?> "blockPP"
+blockPP = wrapBlock BlockPP blockHeader convertPP
   where convertPP :: [Int] -> Parser (Arr PP)
         convertPP vs = do
-          guard (length vs == 7*7) <?> "blockPP with wrong number of parsed integers (" ++ show (length vs) ++ ")"
+          let l = length vs
+          guard (l == 7*7) <?> "expected 7*7=49 entries for blockPP but got " ++ show l
           return $ fromAssocs minPP maxPP (DekaG 999999) [error "blockPP"]
         blockHeader = ["stack"]
 
 blockPBB :: Parser Block
-blockPBB = (\(t,e) vs -> BlockPBB t e vs) <$> (try $ headerParser blockHeader) <*> (pnnArray >>= convertPBB) <?> "blockPBB"
+blockPBB = wrapBlock BlockPBB blockHeader convertPBB
   where convertPBB vs = do
-          guard (length vs == 7*5*5) <?> "blockPBB with wrong number of parsed integers (" ++ show (length vs) ++ ")"
+          let l = length vs
+          guard (l == 7*5*5) <?> "expected 7*5*5=175 entries for blockPBB but got " ++ show l
           return $ fromAssocs minPBB maxPBB (DekaG 999999) [error "blockPBB"]
         blockHeader = ["mismatch_hairpin", "mismatch_interior", "mismatch_interior_1n", "mismatch_interior_23", "mismatch_multi", "mismatch_exterior"]
+
+blockPB :: Parser Block
+blockPB = wrapBlock BlockPB blockHeader convertPB
+  where convertPB vs = do
+          let l = length vs
+          guard (l == 7*5) <?> "7*5=35 entries for blockPB but got " ++ show l
+          return $ fromAssocs minPB maxPB (DekaG 999999) [error "blockPB"]
+        blockHeader = ["dangle5", "dangle3"]
+
+blockPPBB :: Parser Block
+blockPPBB = wrapBlock BlockPPBB blockHeader convertPPBB
+  where convertPPBB vs = do
+          let l = length vs
+          guard (l == 7*7*5*5) <?> "7*7*5*5=1225 entries for blockPPBB but got " ++ show l
+          return $ fromAssocs minPPBB maxPPBB (DekaG 999999) [error "blockPPBB"]
+        blockHeader = ["int11"]
+
+blockPPBBB :: Parser Block
+blockPPBBB = wrapBlock BlockPPBBB blockHeader convertPPBBB
+  where convertPPBBB vs = do
+          let l = length vs
+          guard (l == 7*7*5*5*5) <?> "7*7*5*5*5=6125 entries for blockPPBBB but got " ++ show l
+          return $ fromAssocs minPPBBB maxPPBBB (DekaG 999999) [error "blockPPBBB"]
+        blockHeader = ["int21"]
+
+-- | Parses the big @int22@ lists. Excludes special characters and only
+-- includes standard nucleotides / pairs.
+
+blockPPBBBB :: Parser Block
+blockPPBBBB = wrapBlock BlockPPBBBB blockHeader convertPPBBBB
+  where convertPPBBBB vs = do
+          let l = length vs
+          guard (l == 6*6*4*4*4*4) <?> "6*6*4*4*4*4=30625 entries for blockPPBBBB but got " ++ show l
+          return $ fromAssocs minPPBBBB maxPPBBBB (DekaG 999999) [error "blockPPBBBB"]
+        blockHeader = ["int22"]
+
+wrapBlock :: (Text -> EE -> arr -> Block) -> [Text] -> ([Int] -> Parser arr) -> Parser Block
+wrapBlock block blockHeader convert
+  =   (\(t,e) vs -> block t e vs) <$> headerParser blockHeader <*> (arrayLines >>= convert)
+  <?> "wrapBlock"
 
 -- | Try different headers, succeed only if the full line can be parsed.
 -- Required because prefixes of headers overlap.
@@ -61,22 +107,16 @@ headerParser :: [Text] -> Parser (Text,EE)
 headerParser ps = choice [try $ (,) <$ text "#" <* whiteSpace <*> text p <*> pEE | p <- ps]
   where pEE = (Entropy <$ newline) <|> (Enthalpie <$ text "_enthalpies" <* newline)
 
-ppArray :: Parser [Int]
-ppArray = do
+arrayLines :: Parser [Int]
+arrayLines = do
   headerComment <- commentLine
-  valueLines <- count 7 valueLine
+  valueLines <- valueLine `sepEndBy` newline
   return $ concat valueLines
 
 commentLine = optional $ between (text "/*") (text "*/") (spaces *> some (letter <|> oneOf ",") `sepEndBy` spaces) <?> "commentLine"
 
 valueLine :: Parser [Int]
-valueLine = spaces *> (fromInteger <$> integer) `sepEndBy` spaces <* commentLine <* newline
-
-pnnArray :: Parser [Int]
-pnnArray = do
-  headerComment <- commentLine
-  valueLines <- count (7*5) valueLine
-  return $ concat valueLines
+valueLine = spaces *> (fromInteger <$> integer) `sepEndBy` spaces <* commentLine -- <* newline
 
 
 test = fromFile "data/rna_turner2004.par"
