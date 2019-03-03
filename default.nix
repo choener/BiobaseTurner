d96650ee73d2e60b435ef4c7b8b58ae7cedcfba5
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-04 00:15:48 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,30 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, aeson, base, binary, BiobaseTypes, BiobaseXNA
+, bytestring, bytestring-lexing, cereal, cmdargs, containers
+, data-default, filepath, HUnit, lens, primitive, PrimitiveArray
+, QuickCheck, SciBaseTypes, split, stdenv, tasty, tasty-hunit
+, tasty-quickcheck, tasty-th, text, trifecta, vector
+, vector-th-unbox
+}:
+mkDerivation {
+  pname = "BiobaseTurner";
+  version = "0.3.2.1";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    aeson base binary BiobaseTypes BiobaseXNA bytestring
+    bytestring-lexing cereal containers data-default filepath lens
+    primitive PrimitiveArray SciBaseTypes split text trifecta vector
+    vector-th-unbox
+  ];
+  executableHaskellDepends = [
+    aeson base binary bytestring cereal cmdargs containers vector
+  ];
+  testHaskellDepends = [
+    base BiobaseXNA data-default HUnit lens PrimitiveArray QuickCheck
+    tasty tasty-hunit tasty-quickcheck tasty-th
+  ];
+  homepage = "https://github.com/choener/BiobaseTurner";
+  description = "Import Turner RNA parameters";
+  license = stdenv.lib.licenses.gpl3;
+}
