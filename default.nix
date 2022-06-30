327797e3e1da1b96544be925a5262968d8519317
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-06-30 19:37:36 +0200

    fixed up default.nix

--- default.nix
+++ default.nix
@@ -1,30 +1,20 @@
 { mkDerivation, aeson, base, binary, BiobaseTypes, BiobaseXNA
-, bytestring, bytestring-lexing, cereal, cmdargs, containers
-, data-default, file-embed, filepath, fused-effects, HUnit, lens
-, primitive, PrimitiveArray, QuickCheck, SciBaseTypes, split
-, stdenv, tasty, tasty-hunit, tasty-quickcheck, tasty-th, text
-, trifecta, vector, vector-th-unbox
+, bytestring, bytestring-lexing, cereal, containers, data-default
+, file-embed, filepath, fused-effects, lens, lib, parsers
+, primitive, PrimitiveArray, SciBaseTypes, split, text, trifecta
+, vector, vector-th-unbox
 }:
 mkDerivation {
   pname = "BiobaseTurner";
   version = "0.3.2.1";
   src = ./.;
-  isLibrary = true;
-  isExecutable = true;
   libraryHaskellDepends = [
     aeson base binary BiobaseTypes BiobaseXNA bytestring
     bytestring-lexing cereal containers data-default file-embed
-    filepath fused-effects lens primitive PrimitiveArray SciBaseTypes
-    split text trifecta vector vector-th-unbox
-  ];
-  executableHaskellDepends = [
-    aeson base binary bytestring cereal cmdargs containers vector
-  ];
-  testHaskellDepends = [
-    base BiobaseXNA data-default HUnit lens PrimitiveArray QuickCheck
-    tasty tasty-hunit tasty-quickcheck tasty-th
+    filepath fused-effects lens parsers primitive PrimitiveArray
+    SciBaseTypes split text trifecta vector vector-th-unbox
   ];
   homepage = "https://github.com/choener/BiobaseTurner";
-  description = "Import Turner RNA parameters";
-  license = stdenv.lib.licenses.gpl3;
+  description = "The Turner RNA energy model";
+  license = lib.licenses.gpl3Plus;
 }
